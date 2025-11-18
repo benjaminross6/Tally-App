eaf550f6ccb802ff825c5d26f4c9da8c1758cace
Marsh2006 <marshmallowross06@gmail.com>
2025-11-17 16:49:01 -0800

    Initial Commit

---
+++ Tapp/TappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TappApp.swift
+//  Tapp
+//
+//  Created by Ben Ross on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TappTests/TappTests.swift
@@ -0,0 +1,17 @@
+//
+//  TappTests.swift
+//  TappTests
+//
+//  Created by Ben Ross on 11/17/25.
+//
+
+import Testing
+@testable import Tapp
+
+struct TappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
